bf8b854ee625ca450d2170489b69fa965832f6a6
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-11-17 21:23:12 +0200

    add ts config

---
+++ src/components/App/App.tsx
@@ -0,0 +1,28 @@
+import { GlobalStyle } from '../GlobalStyle';
+import { Statistics } from '../Statistics/Statistics';
+import { Profile } from '../Profile/Profile';
+import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
+import user from '../DataBase/user.json';
+import stats from '../DataBase/data.json';
+import friends from '../DataBase/friends.json';
+import transactions from '../DataBase/transactions.json';
+import { Container } from './App.styled';
+
+export const App = () => {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="" items={stats} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+};
--- src/components/Layout/Layout.jsx
+++
@@ -1,4 +0,0 @@
-import { Container } from './Layout.styled';
-export const Layout = ({ children }) => {
-  return <Container>{children}</Container>;
-};
